2527adbba5179286d894a5e5330202ed64f8a9c4
Li Bin <tracer9@163.com>
2016-03-06 15:52:08 +0800

    Add Batch Gradient Descent in HW1.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Introduction
 
 This repo is used for the course "Applied Mathematics in Computer" for PhD 
-candidates. The professor is [Hongxin Zhang](http://www.cad.zju.edu.cn/home/zhx/csmath/doku.php?id=2016). 
+candidates. The professor is [Hongxin Zhang](http://www.cad.zju.edu.cn/home/zhx/)
 
 The course contains following parts: 
 
@@ -17,3 +17,11 @@ For more detail course information, please click the following website.
 ## Update
 
 2016/3/4: Create Homework1
+2016/3/6: Add Batch Gradient Descent in HW1.
+          Found bug:
+		  1. Learning rate is actually difficult to choose. when you choose a
+		  small one, you need relatively long time to convergence when degree 
+		  is small.
+		  when you choose a big one, the cost function just turn out to be 
+		  "nan".
+		
--- hw1.py
+++ hw1.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# TODO: 1. Need To Care About the format.
+# TODO: 2. Need To Add Necessary comment.
+# TODO: 3. Add the self-learned learning rate \alpha
 
 def CreateDataset(N, sigma):
     """
@@ -45,10 +48,30 @@ class CurveFitting:
         # print designMatrix
         return designMatrix
 
+    def CalculateLoss(self, design, theta, y):
+        return np.sum((np.dot(design, theta) - self.y)**2)
+
     def Predict(self):
         self.designX = self.CreateDesignMatrix(self.x)
         self.theta = np.dot(np.dot(np.linalg.inv(
             np.dot(self.designX.T, self.designX)), self.designX.T), self.y)
+
+    def PredictWithTraining(self):
+    # Batch Gradient Descent.
+        alpha = 1e-9
+        self.designX = self.CreateDesignMatrix(self.x)
+        self.theta = np.zeros(self.degree + 1)  # Initialize the parameter vector
+        # J = np.sum((np.dot(self.designX,self.theta)-self.y)**2)  # calculate cost.
+        for i in range(0, 10000):
+            gradient = np.dot(self.designX.T, np.dot(self.designX, self.theta) - self.y )
+            self.theta = self.theta - alpha * gradient
+            if i % 10 == 0:
+                print "epoch %d, loss: %f" % (i, self.CalculateLoss(self.designX, self.theta, self.y))
+        # TODO: Convergence Curve To Be Done.
+
+    def PredictWithRegularization(self):
+        self.designX = self.CreateDesignMatrix(self.x)
+
 
     def PlotFigure(self):
         # For plot, we need relatively more points. Here simply set to 100.
@@ -83,6 +106,7 @@ class CurveFitting:
 
 if __name__ == "__main__":
     x, y = CreateDataset(100, 0.1)  # Sample, sigma
-    model1 = CurveFitting(x, y, 9)  # Degree.
-    model1.Predict()
+    model1 = CurveFitting(x, y, 7)  # Degree.
+    # model1.Predict()
+    model1.PredictWithTraining()
     model1.PlotFigure()
